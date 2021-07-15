0f9105875a16cd56ac2336ebf1b6419d8843d069
Zr99 <38181091+Zr99@users.noreply.github.com>
2021-07-16 03:41:11 +1200

    Update MainActivity.java

--- app/src/main/java/com/example/androidjava/MainActivity.java
+++ app/src/main/java/com/example/androidjava/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.androidjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -21,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // ADD YOUR CODE HERE FOR THE CLICK EVENT
     public void onClickStart(View v){
         Toast.makeText(this, "List Page", Toast.LENGTH_SHORT).show();
         Intent i = new Intent(this, ListActivity.class);
